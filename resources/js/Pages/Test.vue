2b162306a7129031e49fc3af5a93792c138ca839
Yann Vidral banvi <bvidral2014@gmail.com>
2022-06-08 05:48:33 +0100

    integration admin-lte

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,5 +1,121 @@
 <template>
-    <div>
-        <h1>Ma page test</h1>
+  <div class="wrapper">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"
+            ><i class="fas fa-bars"></i
+          ></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="index3.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+      </ul>
+
+     
+    </nav>
+
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <a href="index3.html" class="brand-link">
+        <img
+          src="~admin-lte/dist/img/AdminLTELogo.png"
+          alt="AdminLTE Logo"
+          class="brand-image img-circle elevation-3"
+          style="opacity: 0.8"
+        />
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
+      </a>
+
+      <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img
+              src="~admin-lte/dist/img/user2-160x160.jpg"
+              class="img-circle elevation-2"
+              alt="User Image"
+            />
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">Alexander Pierce</a>
+          </div>
+        </div>
+
+       
+
+        <nav class="mt-2">
+          <ul
+            class="nav nav-pills nav-sidebar flex-column"
+            data-widget="treeview"
+            role="menu"
+            data-accordion="false"
+          >
+            <li class="nav-item menu-open">
+              <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  Starter Pages
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="#" class="nav-link active">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Active Page</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Inactive Page</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                  Simple Link
+                  <span class="right badge badge-danger">New</span>
+                </p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0">Starter Page</h1>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+
+      <div class="content">
+        <div class="container-fluid">
+         
+        </div>
+      </div>
     </div>
+
+   
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-inline">Anything you want</div>
+
+      <strong
+        >Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong
+      >
+      All rights reserved.
+    </footer>
+  </div>
 </template>
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,4 +6,8 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import "admin-lte/plugins/jquery/jquery"
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle"
+import "admin-lte/dist/js/adminlte"
+
 
